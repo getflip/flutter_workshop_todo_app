444cc2bbadf67726c628b0f6888340b82efdabbd
Paulo Bernardes <paulo.bernardes@getflip.com>
2025-05-27 13:44:46 +0100

    feat: Show image and description in Todo card

--- lib/features/todo/presentation/widgets/todo_item.dart
+++ lib/features/todo/presentation/widgets/todo_item.dart
@@ -14,14 +14,42 @@ class TodoItem extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          spacing: 8,
           children: [
-            Text(todo.title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 4),
-            Text(
-              todo.formattedDate,
-              style: TextStyle(fontSize: 12, color: Colors.grey[600], fontStyle: FontStyle.italic),
+            SizedBox(
+              height: 50,
+              width: 50,
+              child:
+                  todo.imageUrl != null && todo.imageUrl!.isNotEmpty
+                      ? Image.network(todo.imageUrl!, fit: BoxFit.cover)
+                      : Image.asset('assets/placeholder.webp'),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    todo.title,
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  todo.description != null && todo.description!.isNotEmpty
+                      ? Text(
+                        todo.description!,
+                        style: const TextStyle(fontSize: 14),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                      : const SizedBox(),
+
+                  const SizedBox(height: 4),
+                  Text(
+                    todo.formattedDate,
+                    style: TextStyle(fontSize: 12, color: Colors.grey[600], fontStyle: FontStyle.italic),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
